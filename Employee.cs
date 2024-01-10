54e1bc04f3172e45518eaa80224dc73233f97b37
mayank5654 <mayankkaushal99@gmail.com>
2024-01-10 09:47:24 +0530

    intial commit

---
+++ Employee.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp2
+{
+    internal class Employee
+    {
+        public int id {  get; set; }
+        public string ?name { get; set; }
+        public string? designation{ get; set; }
+        public DateTime doj{ get; set; }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿// See https://aka.ms/new-console-template for more information
+using ConsoleApp2;
+
+Console.WriteLine("Hello, World!");
+List<Employee> employees = new List<Employee>() {
+new Employee() { id = 1, name = "sam", designation = "developer", doj = new DateTime(day: 12, month: 09, year: 2022) },
+new Employee() { id = 2, name = "jam", designation = "tester", doj = new DateTime(day: 15, month: 11, year: 2020) },
+new Employee() { id = 3, name = "ram", designation = "marketing", doj = new DateTime(day: 19, month: 10, year: 2023) },
+new Employee() { id = 4, name = "pam", designation = "developer", doj = new DateTime(day: 10, month: 12, year: 2021) },
+};
+Console.WriteLine("id\t name\t designation \t date of joining");
+foreach(Employee emp in employees)
+{
+    Console.Write(emp.id+"\t \t");
+    Console.Write(emp.name+"\t \t");
+    Console.Write(emp.designation + "\t \t");
+    Console.Write(emp.doj.ToLongDateString());
+    Console.WriteLine("\n");
+    Console.WriteLine("\n");
+}
+Console.ReadKey();
